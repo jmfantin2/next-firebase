bf821c998a9bbd0c4c7d093cf73109dc6ca29134
JM Fantin <jmfantin@outlook.com>
2022-07-03 23:08:31 -0300

    ts + advanced configs

---
+++ components/gears/GButton.tsx
@@ -0,0 +1,23 @@
+import { GoogleLogo } from "phosphor-react";
+
+interface GButtonProps {
+  click: React.MouseEventHandler;
+}
+
+export function GButton(props: GButtonProps) {
+  return (
+    <a
+      onClick={props.click}
+      href="#"
+      className={
+        "group bg-white hover:bg-slate-100 text-blue-600 transition-colors text-sm py-2 px-1  rounded-full max-h-12 font-bold uppercase"
+      }
+    >
+      <p className="flex flex-row items-center ml-2 mr-3 group-hover:text-green-500 transition-colors">
+        <GoogleLogo size={24} weight="bold" className="mr-2" />
+        Entre com Google
+      </p>
+      {/*consume token for logout*/}
+    </a>
+  );
+}
