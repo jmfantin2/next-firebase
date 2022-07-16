76b78b6dd10e15eb67e682d25addd4c969336873
JM Fantin <jmfantin@outlook.com>
2022-07-16 09:10:49 -0300

    both tables

--- components/DRETable.tsx
+++ components/DRETable.tsx
@@ -1,28 +1,74 @@
+import { Calendar } from "phosphor-react";
+
 export function DRETable() {
   return (
     <table className="tw_table">
       <thead>
         <tr>
-          <th>Song</th>
-          <th>Artist</th>
-          <th>Year</th>
+          <th>CNPJ</th>
+          <th>
+            Data
+            <br />
+            Inicial
+          </th>
+          <th>
+            Data
+            <br />
+            Final
+          </th>
+          <th>
+            Custo
+            <br />
+            Fixo
+          </th>
+          <th>
+            M. Contribuição
+            <br />
+            Nominal Total
+          </th>
+          <th>
+            Faturamento
+            <br />
+            Total
+          </th>
+          <th>
+            Custos
+            <br />
+            Variáveis
+          </th>
+          <th>Lucro</th>
+          <th>
+            Ponto de
+            <br />
+            Equilíbrio
+          </th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-          <td>Malcolm Lockyer</td>
-          <td>1961</td>
-        </tr>
-        <tr>
-          <td>Witchy Woman</td>
-          <td>The Eagles</td>
-          <td>1972</td>
-        </tr>
-        <tr>
-          <td>Shining Star</td>
-          <td>Earth, Wind, and Fire</td>
-          <td>1975</td>
+          <td>96.245.212/0001-08</td>
+          <td>
+            2022-05-01{" "}
+            {
+              <button>
+                <Calendar size={20} color="#337aff" />
+              </button>
+            }
+          </td>
+          <td>
+            2022-05-30{" "}
+            {
+              <button>
+                <Calendar size={20} color="#337aff" />
+              </button>
+            }
+          </td>
+          <td>200.0</td>
+          <td>341.55</td>
+          <td>611.99</td>
+          <td>270.44</td>
+          <td>141.55</td>
+          <td>358.36</td>
         </tr>
       </tbody>
     </table>
--- components/ProdTable.tsx
+++ components/ProdTable.tsx
@@ -3,20 +3,49 @@ export function ProdTable() {
     <table className="tw_table">
       <thead>
         <tr>
-          <th>Produto</th>
-          <th>Situação Venda</th>
+          <th>ID</th>
+          <th>
+            Situação
+            <br />
+            Venda
+          </th>
           <th>Despesas</th>
-          <th>Margem Nominal</th>
-          <th>Margem Percentual</th>
+          <th>
+            Margem
+            <br />
+            Nominal
+          </th>
+          <th>
+            Margem
+            <br />
+            Percentual
+          </th>
           <th>Custo</th>
-          <th>Preço Mínimo</th>
-          <th>Preço Visado</th>
-          <th>Quantidade Vendida</th>
+          <th>
+            Preço
+            <br />
+            Mínimo
+          </th>
+          <th>
+            Preço
+            <br />
+            Venda
+          </th>
+          <th>
+            Margem
+            <br />
+            Contribuição
+          </th>
+          <th>
+            Quantidade
+            <br />
+            Vendida
+          </th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td>Caneta Vermelha</td>
+          <td>1</td>
           <td>Shopee</td>
           <td>13.00%</td>
           <td>R$ 0.00</td>
@@ -24,28 +53,31 @@ export function ProdTable() {
           <td>R$ 1.80</td>
           <td>R$ 2.07</td>
           <td>R$ 2.07</td>
+          <td>R$ 0.00</td>
           <td>1</td>
         </tr>
         <tr>
-          <td>Caneta Vermelha</td>
-          <td>Shopee</td>
+          <td>2</td>
+          <td>Mercado Livre</td>
           <td>13.00%</td>
           <td>R$ 0.00</td>
           <td>0.00%</td>
           <td>R$ 1.80</td>
           <td>R$ 2.07</td>
           <td>R$ 2.07</td>
+          <td>R$ 0.00</td>
           <td>1</td>
         </tr>
         <tr>
-          <td>Caneta Vermelha</td>
-          <td>Shopee</td>
+          <td>3</td>
+          <td>Casa do Papel</td>
           <td>13.00%</td>
           <td>R$ 0.00</td>
           <td>0.00%</td>
           <td>R$ 1.80</td>
           <td>R$ 2.07</td>
           <td>R$ 2.07</td>
+          <td>R$ 0.00</td>
           <td>1</td>
         </tr>
       </tbody>
