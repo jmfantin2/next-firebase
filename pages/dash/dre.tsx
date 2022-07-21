884b4d214c043183eaf00012af91e3720e93e49c
JM Fantin <jmfantin@outlook.com>
2022-07-21 16:47:59 -0300

    table styling refactor 6

--- pages/dash/dre.tsx
+++ pages/dash/dre.tsx
@@ -3,8 +3,10 @@ import { DRETable } from "../../components/DRETable";
 export default function DREPage({}) {
   return (
     <main className="tw_container">
-      <DRETable />
-      <div className="tw_tableoptions">
+      <div className="flex flex-wrap justify-center">
+        <DRETable />
+      </div>
+      <div className="tw_dre_table_sub">
         <button>Simular</button>
         <button>Salvar</button>
       </div>
